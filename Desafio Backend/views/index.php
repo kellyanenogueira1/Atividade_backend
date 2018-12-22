fa175233fdac9d06d30d3b37cee74827e15fdc6c
Kellyane <kellyanenogueira1@gmail.com>
2018-12-21 22:07:57 -0200

    Criando layout da página inicial

---
+++ Desafio Backend/views/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Logar</title>
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </head>
+
+        <nav class="navbar navbar-fixed-top navbar-default" >
+        	<div class="container">
+				<div class="navbar-header"></div>
+			</div>
+		</nav>
+		
+        <br/>
+        <div class="container">
+	    	
+	    	<br /><br />
+	    	<div class="col-md-4"></div>
+
+	    	<div class="col-md-4">
+	    		<h3>Faça Login:</h3>
+	    	
+				<form method="post" action="" id="formLogin">
+					<div class="form-group">
+						<input type="text" class="form-control" id="usuario" name="usuario" placeholder="Usuário" required="requiored">
+					</div>
+                    
+					<div class="form-group">
+						<input type="password" class="form-control" id="senha" name="senha" placeholder="Senha" required="requiored">
+					</div>
+					
+					<button type="submit" class="btn btn-primary form-control">Entrar</button>
+				</form>
+				<br />
+            </div>
+        </div>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <body>
+</html>
