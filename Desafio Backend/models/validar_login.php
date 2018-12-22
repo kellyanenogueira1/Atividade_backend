18c1f00dd6e558fc255773432f389c271b625439
Kellyane <kellyanenogueira1@gmail.com>
2018-12-21 22:31:11 -0200

    Tratamento de erro

--- Desafio Backend/models/validar_login.php
+++ Desafio Backend/models/validar_login.php
@@ -11,4 +11,10 @@
     $conexao = $object->Connection();
 
     $result = mysqli_query($conexao, $sql);
+
+    if($result){
+        $dados_login = mysqli_fetch_array($result); //Transformando uma referência em array
+    } else {
+        header('Location: ../views/index.php?erro=1'); //Forçando um redirecionamento para o index.php
+    }
 ?>
